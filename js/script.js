a9d28826f910298a1c08aedb2ebb85962dd172b3
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-04-27 14:00:17 +0000

    added code for webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+const randomNumber = Math.floor(Math.random() * 6) + 1 
+
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt (document.getElementById("inputValue").value)
+
+  // process
+  if (numberGuessed == randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "You got it!"
+  }
+  if (numberGuessed != randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "Sorry, you are incorrect."
+  }
+}
